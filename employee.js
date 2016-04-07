51d303d13ddd22768969029d9c9c70b6bc81cb1b
Brandon Cornel <brandbread@gmail.com>
2016-04-07 12:05:14 -0400

    Added skeleton API stuff

---
+++ employee.js
@@ -0,0 +1,14 @@
+app.get("/employees", function(req, res) {
+});
+
+app.post("/employees", function(req, res) {
+});
+
+app.get("/employees/:id", function(req, res) {
+});
+
+app.put("/employees/:id", function(req, res) {
+});
+
+app.delete("/employees/:id", function(req, res) {
+});
