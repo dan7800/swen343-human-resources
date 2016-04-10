1fcd7a8633d54b021e20c5fe072fcfa2ee2c78cd
Brandon Cornel <brandbread@gmail.com>
2016-04-10 11:44:13 -0400

    Added employee schema

--- employee.js
+++ employee.js
@@ -1,4 +1,22 @@
+var mongoose = reqire('mongoose');
+
+mongoose.connect('mongodb://localhost/employees');
+
+var employeeSchema = new mongoose.Schema({
+	name: String,
+	address: String,
+	age: Number
+	phoneNumber: String
+	department: String,
+	salary: Number
+})
+
+var employee = mongoose.model('Employee', employeeSchema)
+
+
 app.get("/employees", function(req, res) {
+
+	
 });
 
 app.post("/employees", function(req, res) {
