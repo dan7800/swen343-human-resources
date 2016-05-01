d7b5f624eaaf0e7116ad0cd184f641ad063c46ca
Nick James <nxj2348@rit.edu>
2016-05-01 17:57:42 -0400

    added findByEmployee function in the payrollSchema. added field to payrollSchema to indicate the latest payroll for an employee

--- payroll/payrollSchema.js
+++ payroll/payrollSchema.js
@@ -4,10 +4,15 @@
 var mongoose = require('mongoose');
 
 var payrollSchema = new mongoose.Schema({
-    employeeId: Long,
+    employeeId: Number,
     paycheckAmount: Number,
     datePaid: Date,
-    lastModified: Date
+    lastModified: Date,
+    lastest: Boolean
 });
+
+this.statics.findByEmployeeId = function (id, cb) {
+    return this.find({ employeeId: id, latest: true}, cb);
+};
 
 module.exports = mongoose.model('Payroll', payrollSchema);
--- tests/test-payroll.js
+++ tests/test-payroll.js
@@ -1,11 +1,12 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/employees');
 var server = require('../server');
 var should = chai.should();
 var assert = chai.assert;
 
-var payroll = require('../payroll/payroll');
+var payroll = require(payrollSchema);
 
 chai.use(chaiHttp);
 
@@ -39,7 +40,7 @@ it('Paying an employee should create a new entry in the payroll table.', functio
 
     Payroll.payEmployee(1);
 
-    payroll.findById(1, function (err, doc){
-        //assert(doc.)
+    payroll.findByEmployeeId(1, function (err, doc){
+        assert(doc.paycheckAmount, (req.body.salary/52));
     });
 });
