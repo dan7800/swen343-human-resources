bd05cebabd48f3896c64af8d9c15d96ca89d9704
Noah Frank <udadaadaa@gmail.com>
2016-05-06 19:04:38 -0400

    -Moved index.html to fit file structure -Moved functionality from timesheetSchema to timesheet -Fixed timesheet database interactions -Changed empID to a employee firstname/lastname/DOB because empIDs are very hard to put into a form

--- timesheets/timesheet.js
+++ timesheets/timesheet.js
@@ -3,22 +3,38 @@
  */
 var mongoose = require('mongoose');
 var timeSheetModel = require('timesheetSchema');
-
+var employeeModel = require('../employees/employeeSchema');
 
 module.exports = {
-    calculatePay: function (mon, tues, wed, thurs, fri, sat, sun, empID) {
-        var total = mon+tues+wed+thurs+fri+sat+sun;
-        var info = {
-            'total':total,
-            'employeeID':empID,
-            'dateCreated':Date.now()
-        };
-        timeSheetModel.insert(total);
+    calculateAndStorePay: function (mon, tues, wed, thurs, fri, sat, sun, firstName, lastName, dob) {
+        // Sum to determine total hours worked
+        var totalHours = mon+tues+wed+thurs+fri+sat+sun;
+        // Get the employee id
+        employeeModel.findOne({'firstName': firstName, 'lastName': lastName, 'dob': new Date(dob)}, function (err, emp) {
+            if (err) {
+                console.log("Error trying to find the given employee: ", err);
+                return false;
+            } else if (emp == null) {
+                console.log("Could not find the given employee");
+                return false;
+            } else {
+                // Create a new timesheet with this data and save it
+                var newTimeSheet = new timeSheetModel();
+                newTimeSheet.total = totalHours;
+                newTimeSheet.employeeID = emp._id;
+                newTimeSheet.dateCreated = Date.now();
+
+                newTimeSheet.save(function (err) {
+                    if (err) {
+                        console.log("Error saving timesheet in calculateAndStorePay");
+                        console.log(err);
+                    }
+                });
+            }
+        });
     },
 
-
-
-    deleteEntry: function (date, employeeID) {
+    deleteEntry: function (employeeID, date) {
         var selector = {
             'dateCreated': date,
             'employeeID': employeeID
@@ -26,10 +42,29 @@ module.exports = {
         timeSheetModel.remove(selector)
     },
 
-    getHoursByEmployeeId: function (id) {
-        return mongoose.model('')
-            .findById(id)
-            .select('salary')
+    /**
+     * Returns all the timesheets associated with the given employee id
+     * @param employeeID - Employee to get timesheet history for
+     * @param cb - Callback function which has the parameters (err, listOfTimesheets),
+     * where listOfTimesheets is the result of the query
+     */
+    getHoursByEmployeeId: function (employeeID, cb) {
+        return timeSheetModel
+            .find({'employeeId': employeeID})
+            .exec(cb);
+    },
+
+    getByEmployeeIdAndDateCreated: function (id, date, cb) {
+        return timeSheetModel
+            .where('employeeId', id)
+            .where('dateCreated', new Date(date))
+            .exec(cb);
+    },
+
+    getLatestForEmployee: function (id, cb) {
+        return timeSheetModel
+            .findOne()
+            .sort({lastModified: -1})
             .exec(cb);
     }
 };
--- timesheets/timesheetSchema.js
+++ timesheets/timesheetSchema.js
@@ -6,21 +6,7 @@
 var timesheetSchema = new mongoose.Schema({
     employeeId: String,
     total: Number,
-    dateCreated: Date,
+    dateCreated: Date
 });
-
-timesheetSchema.statics.getByEmployeeIdAndDateCreated = function (id, date, cb) {
-    return mongoose.model('Timesheet')
-        .where('employeeId', id)
-        .where('dateCreated', date)
-        .exec(cb);
-};
-
-timesheetSchema.statics.getLatestForEmployee = function (id, cb) {
-    return mongoose.model('Timesheet')
-        .where('employeeId', id)
-        .where('latest', true)
-        .exec(cb);
-};
 
 module.exports = mongoose.model("Timesheet", timesheetSchema);
