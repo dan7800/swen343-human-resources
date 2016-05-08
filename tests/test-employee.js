1e7119c59d5db695d8301733bf021b7a6a10884b
Brandon Cornel <brandbread@gmail.com>
2016-05-08 10:38:34 -0400

    Added another employee validation test to test-employee.js

--- tests/test-employee.js
+++ tests/test-employee.js
@@ -61,30 +61,29 @@ it('/createEmployee should create a new employee in the mongodb database', funct
 });
 
 it('/createEmployee should not create a new employee in the mongodb database', function(done) {
-    var req = {};
-    req.firstName = "";
-    req.lastName = "";
-    req.position = "SE";
-    req.department = "Software Development";
-    req.streetAddress = "53 Tucan Lane";
-    req.city = "Rochester";
-    req.state = "New York";
-    req.zipCode = 14623;
-    req.gender = "Male";
-    req.DOB = "1995-05-18";
-    req.phone = "124-321-5234";
-    req.salary = 100000;
+var employee = new employeeModel();
 
-    chai.request(server)
-        .post('/createEmployee')
-        .send(req)
-        .end(function(err, res) {
-            res.should.have.status(200);
-            employeeModel.findOne({ 'firstName': req.firstName, 'lastName': req.lastName }, function (err, emp) {
-                if (err) assert(True, "Employee was not created" + err);
-                done();
-            })
-        });
+    employee.firstName = "";
+    employee.lastName = "";
+    employee.position = "SE";
+    employee.department = "Software";
+    employee.street = "607 Park Point";
+    employee.city = "Rochester";
+    employee.state = "NY";
+    employee.zipcode = 14623;
+    employee.gender = "Male";
+    employee.dob = "1995-08-18";
+    employee.phone = "123-456-7890";
+    employee.salary = 100000;
+    employee.lastModified = new Date();
+
+    employee.save(function(err, employee) { // Wait for employee entry to be saved and returned
+        if (err){
+            assert(true, "Employee was not created:" + err);
+        }
+        done();
+    });
+        
 });
 
 
