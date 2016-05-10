873e78d873fb62a0fc9dedf1c53afa133b294791
Nick James <nxj2348@rit.edu>
2016-05-10 11:57:20 -0400

    hopefully fixed crash issue

--- payroll/payroll.js
+++ payroll/payroll.js
@@ -23,7 +23,7 @@ var postToAccounting = module.exports.postToAccounting = function(id, paycheck) 
             qs: { apiKey: 'human_resources' },
             headers:
             {  'cache-control': 'no-cache' },
-            body: {pay: paycheck, desc: desc}};
+            body: JSON.stringify({pay: paycheck, desc: desc})};
 
         request(options, function (error, response, body) {
             if (error) throw new Error(error);
